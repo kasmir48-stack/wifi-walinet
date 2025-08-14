7240684fcb1a94e09eed1f7f0c4fad2d6fa226e1
kasmir48-stack <kasmir48@guru.smp.belajar.id>
2025-08-14 22:04:56 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="wifi-oneurl-v2";
+const CACHE="wifi-oneurl-v7";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
@@ -8,3 +8,4 @@ self.addEventListener("fetch",e=>{
     e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
   }
 });
+
